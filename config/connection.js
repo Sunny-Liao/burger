95e3f8cbe1a85ec6bfb01e505023e5b2f36997ab
Sunny-Liao <sunnylia@gmail.com>
2019-03-31 02:33:10 -0500

    burger app

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     user: "root",
     password: "password",
     database: "burger_db",
-    port:"8889",
+    port:"3306",
     socket:"/Applications/MAMP/tmp/mysql/mysql.sock"
 })
 
