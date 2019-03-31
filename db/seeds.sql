743da1a8366d652c2d83abee97785ddb83265129
Sunny Liao <43971988+Sunny-Liao@users.noreply.github.com>
2019-03-31 00:44:31 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with Cheese Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom & Swiss Burger', false);
+
